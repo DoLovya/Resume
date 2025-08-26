ba5822023efb88632c2103dedda0950a0ce61a3f
DoLovya <3063676257@qq.com>
2025-08-26 23:48:27 +0800

    🎉 初始化项目

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Upload, Avatar as AntdAvatar } from 'antd';
+import './index.less';
+import defaultAvatar from '../../../static/images/defaultAvatar.jpg';
+
+export const Avatar = ({
+  avatarSrc,
+  className,
+  shape = 'circle',
+  size = 'default',
+}) => {
+  return (
+    <div className={`avatar ${!avatarSrc ? 'avatar-hidden' : ''}`}>
+      {avatarSrc ? (
+        // @ts-ignore
+        <AntdAvatar
+          className={className}
+          src={avatarSrc}
+          shape={shape as any}
+          size={size as any}
+        />
+      ) : (
+        <AntdAvatar
+          className={className}
+          src={defaultAvatar}
+          shape={shape as any}
+          size={size as any}
+        />
+      )}
+    </div>
+  );
+};
